667ab3420281e1df10297fc1a7f24bdaca281033
Arian Ansari <esfahanarian@gmail.com>
2023-12-03 17:24:18 +0100

    css updated

--- src/components/booking/index.tsx
+++ src/components/booking/index.tsx
@@ -1,76 +1,73 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import Button from "../button";
 
-import "./index.sass"
+import "./index.sass";
 
 const options = [
-    {
-        id: 2,
-        text: "Get your class 5 or class 7 road test booking within 4 weeks.",
-        heading: "Within 4 weeks",
-        price: 19.95,
-        value: "4 weeks"
-    },
-    {
-        id: 1,
-        text: "Get your class 5 or class 7 road test booking within 1 week.",
-        heading: "Within 1 week",
-        price: 39.95,
-        value: "1 week"
-    },
-]
+  {
+    id: 2,
+    text: "Get your class 5 or class 7 road test booking within 4 weeks.",
+    heading: "Within 4 weeks",
+    price: 19.95,
+    value: "4 weeks",
+  },
+  {
+    id: 1,
+    text: "Get your class 5 or class 7 road test booking within 1 week.",
+    heading: "Within 1 week",
+    price: 39.95,
+    value: "1 week",
+  },
+];
 
 export default function Booking(props: any) {
-    const [ activeID, setActiveID ] = useState(1)
+  const [activeID, setActiveID] = useState(1);
 
-    const handleOptionSelect = (id: number) => {
-        if (id === activeID) {
-
-            return;
-        }
-
-        // const selectedOption = options.filter(option => option.id === id)[0];
-
-        setActiveID(id);
+  const handleOptionSelect = (id: number) => {
+    if (id === activeID) {
+      return;
     }
 
-    return (
-        <section id="book">
-            <div className="columns">
-                <div className="container book-paper">
-                    <h3>Book now</h3>
-                    <h2>Choose the ideal package for you and get your road test soon.</h2>
-                    <div className="book-options-container flex row med-column between">
-                        {options.map(
-                            (option, i) => {
-                            const isActive = option.id === activeID;
-                            const className = isActive ? "active" : "";
-                            return (
-                                <div 
-                                    className={`option ${className}`}
-                                    key={`option-${option.id}`} 
-                                    onClick={() => handleOptionSelect(option.id)}
-                                >
-                                    <h4>{option.heading}</h4>
-                                    <p>{option.text}</p>
-                                    <div className="price">{option.price}</div>
-                                </div>
-                            )
+    // const selectedOption = options.filter(option => option.id === id)[0];
 
-                        })}
-                    </div>
-                    <div className="book-footer flex row between align-center wrap-reverse">
-                        <p>If we fail to deliver, then you will receive a <u>full refund.</u></p>
-                        <Link to={`/checkout?opt=${activeID}`}>
-                            <Button className="booking-button">
-                                Book now
-                            </Button>
-                        </Link>
-                    </div>
+    setActiveID(id);
+  };
+
+  return (
+    <section id="book">
+      <div className="columns">
+        <div className="container book-paper">
+          <h3>Book now</h3>
+          <h2>Choose the ideal package for you and get your road test soon.</h2>
+          <div className="book-options-container flex row med-column between">
+            {options.map((option) => {
+              const isActive = option.id === activeID;
+              const className = isActive ? "active" : "";
+              return (
+                <div
+                  className={`option ${className}`}
+                  key={`option-${option.id}`}
+                  onClick={() => handleOptionSelect(option.id)}
+                >
+                  <h4>{option.heading}</h4>
+                  <p>{option.text}</p>
+                  <div className="price">{option.price}</div>
                 </div>
-            </div>
-        </section>
-    )
+              );
+            })}
+          </div>
+          <div className="book-footer flex row between align-center wrap-reverse">
+            <p>
+              If we fail to deliver, then you will receive a <u>full refund.</u>
+            </p>
+            <Link to={`/checkout?opt=${activeID}`}>
+              <Button className="booking-button">Book now</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,43 +1,50 @@
-import React from 'react'
-import "./index.sass"
-import Button from "../button"
-import { Link } from "react-router-dom"
+import "./index.sass";
+import Button from "../button";
+import { Link } from "react-router-dom";
 
 interface Props {
-    showModal?: boolean,
-    handleModal: (bool: boolean) => void
+  showModal?: boolean;
+  handleModal: (bool: boolean) => void;
 }
 
 export default function Login(props: Props) {
-
-    const renderAccountForm = 
+  const renderAccountForm = (
     <>
-        <h3>Driver's Last name</h3>
-        <input type="text" className="input" title="Last Name" autoComplete="family-name"/>
-        <h3>BC driver's licence number</h3>
-        <input type="text" className="input" title="Licence Number"/>
-        <h3>Keyword</h3>
-        <input type="text" className="input" title="Keyword"/>
+      <h3>Driver's Last name</h3>
+      <input
+        type="text"
+        className="input"
+        title="Last Name"
+        autoComplete="family-name"
+      />
+      <h3>BC driver's licence number</h3>
+      <input type="text" className="input" title="Licence Number" />
+      <h3>Keyword</h3>
+      <input type="text" className="input" title="Keyword" />
     </>
+  );
 
-
-    return (
-        <div className={`modal ${props.showModal ? 'show' : ''}`} onClick={() => props.handleModal(false)}>
-            <div className="modal-content" onClick={e => e.stopPropagation()}>
-                <div className="modal-header">
-                    <h2 className="modal-title">Sign In</h2>
-                </div>
-                <div className="modal-body">
-                    {renderAccountForm}
-                </div>
-                <div className="modal-footer">
-                    <Link to="/dashboard">
-                        <Button variant="secondary" onClick={() => props.handleModal(false)}>
-                            Sign in
-                        </Button>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div
+      className={`modal ${props.showModal ? "show" : ""}`}
+      onClick={() => props.handleModal(false)}
+    >
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <div className="modal-header">
+          <h2 className="modal-title">Sign In</h2>
         </div>
-    )
+        <div className="modal-body">{renderAccountForm}</div>
+        <div className="modal-footer">
+          <Link to="/dashboard">
+            <Button
+              variant="secondary"
+              onClick={() => props.handleModal(false)}
+            >
+              Sign in
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
